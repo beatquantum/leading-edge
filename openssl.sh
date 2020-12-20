b6a81514accde0551e345029f32713f37f482a71
beatquantum <67234356+beatquantum@users.noreply.github.com>
2020-12-20 18:59:43 +0000

    Update and rename 01-OpenSSL.sh to openssl.sh
    Updating to openssl 1.1.1i (instead of 1.1.1h before)

--- openssl.sh
+++ openssl.sh
@@ -5,30 +5,34 @@ echo "*******************************************************************"
 echo "* Source: https://github.com/beatquantum/leading-edge.git         *"
 echo "* Author: Santosh Pandit                                          *"
 echo "*******************************************************************"
-echo "* Updating to OpenSSL version 1.1.1h                              *"
+echo "Here is your existing OpenSSL version                             *"
+openssl version
+echo "*******************************************************************"
+echo "* Updating to OpenSSL version 1.1.1i                              *"
 echo "* Enter 1 to continue, or Ctrl-C to abort                         *"
 read n
 if [ $n -eq 1 ];
 then
-echo "* Installing dependencies... (this will take a minute)            *"
-apt-get update
-apt-get install make -y 
-apt-get install gcc -y 
+echo "* Step 1 - Running apt update *"
+apt-get update > /dev/null
+echo "* Step 2 - Installing make and gcc *"
+apt-get install make gcc -y > /dev/null
 cd /usr/src  
-echo "* Downloading OpenSSL files...(this will take a minute)           *"
+echo "* Step 3 - Downloading OpenSSL files *"
 echo "* Source: https://www.openssl.org                                 *"
-sudo wget https://www.openssl.org/source/openssl-1.1.1h.tar.gz --no-check-certificate
-sudo tar -zxf openssl-1.1.1h.tar.gz 
-cd openssl-1.1.1h 
+sudo wget https://www.openssl.org/source/openssl-1.1.1i.tar.gz --no-check-certificate > /dev/null
+sudo tar -zxf openssl-1.1.1i.tar.gz > /dev/null
+cd openssl-1.1.1i > /dev/null
+echo "* Step 4 - Running config *"
 ./config > /dev/null
-echo "* Installing OpenSSL...(have patience!)                           *"
-echo "* Running make...                                                 *"
-make 
-echo "* Running make test...                                            *"
-make test 
-echo "* Running make install...                                         *"
+echo "* Step 5 - Installing OpenSSL *"
+echo "* Running make...*"
+make > /dev/null
+echo "* Running make test...*"
+make test > /dev/null
+echo "* Running make install...*"
 make install
-echo "* Completing the links...                                         *"
+echo "* Step 6 - Completing the links...*"
 echo "/usr/local/lib64" > /etc/ld.so.conf.d/openssl.conf
 ldconfig > /dev/null
 echo "* Reboot at your convenience and check with openssl version       *"
