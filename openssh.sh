f71e94ebea45cf01c36447145d1be1a9d41c5de2
beatquantum <67234356+beatquantum@users.noreply.github.com>
2020-12-20 19:32:42 +0000

    Update and rename 02-OpenSSH.sh to openssh.sh

---
+++ openssh.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "*******************************************************************"
+echo "* Always use every script thoroughly in a test environment first. *"
+echo "*******************************************************************"
+echo "* Source: https://github.com/beatquantum/leading-edge.git         *"
+echo "* Author: Santosh Pandit                                          *"
+echo "*******************************************************************"
+echo "* Your current OpenSSH version is the following                   *"
+ssh -V
+echo "*******************************************************************"
+echo "* Updating to OpenSSH version 8.4p1                               *"
+echo "* Source: https://www.openssh.com                                 *"
+echo "* Enter 1 to continue, or Ctrl-C to abort                         *"
+read n
+if [ $n -eq 1 ];
+then
+echo "* Step 1 : Installing required package dependencies...*"
+sudo apt-get update > /dev/null
+sudo apt-get install build-essential zlib1g-dev libssl-dev libpam0g-dev libselinux1-dev -y > /dev/null
+mkdir /var/lib/sshd > /dev/null
+chmod -R 700 /var/lib/sshd/ > /dev/null
+useradd -r -U -d /var/lib/sshd/ -c "sshd privsep" -s /bin/false sshd > /dev/null
+echo "*(Ignore if you saw an error message- user 'sshd' already exists. *"
+echo "* Step 2 : Downloading OpenSSH...*"
+wget -c --no-check-certificate https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.4p1.tar.gz > /dev/null
+tar -xzf openssh-8.4p1.tar.gz  > /dev/null
+cd openssh-8.4p1/  > /dev/null
+echo "* Step 3 : Configuring SSH without PAM *"
+./configure --with-md5-passwords --with-selinux --with-privsep-path=/var/lib/sshd/ --sysconfdir=/etc/ssh > /dev/null
+echo "* Step 4 : Installing OpenSSH...(have patience!) *"
+echo "* Running make... *"
+make > /dev/null
+echo "* Running make install... *"
+make install > /dev/null
+echo "* Reboot at your convenience and check the kernel using ssh -V    *"
+echo "*******************************************************************"
+fi
