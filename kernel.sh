c534ca418c643372fce672d9f259088941ad4e37
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2020-12-28 16:46:30 +0000

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -19,7 +19,7 @@ echo "* Step 1 : Downloading Linux kernel files...(this will take a minute)     
 wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/amd64/linux-headers-5.10.3-051003-generic_5.10.3-051003.202012261630_amd64.deb > /dev/null
 wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/amd64/linux-headers-5.10.3-051003_5.10.3-051003.202012261630_all.deb > /dev/null
 wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/amd64/linux-image-unsigned-5.10.3-051003-generic_5.10.3-051003.202012261630_amd64.deb > /dev/null
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/amd64/linux-image-unsigned-5.10.3-051003-generic_5.10.3-051003.202012261630_amd64.deb > /dev/null
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/amd64/linux-modules-5.10.3-051003-generic_5.10.3-051003.202012261630_amd64.deb > /dev/null
 echo "* Step 2 : Updating Linux kernel...(this will take a couple of minutes)    *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
