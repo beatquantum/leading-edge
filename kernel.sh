b8f862ea36acb3259c91f3b5067a67d3899a1226
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2020-12-28 16:44:13 +0000

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 clear
 echo "*******************************************************************"
 echo "* Always use every script thoroughly in a test environment first. *"
@@ -9,17 +9,17 @@ echo "*******************************************************************"
 echo "* Your current kernel is below                                    *"
 uname -a
 echo "*******************************************************************"
-echo "* Updating to Linux kernel version 5.10.2                          *"
+echo "* Updating to Linux kernel version 5.10.3                          *"
 echo "* Enter 1 to continue, or Ctrl-C to abort                         *"
 read n
 if [ $n -eq 1 ];
 then
 cd /tmp > /dev/null
 echo "* Step 1 : Downloading Linux kernel files...(this will take a minute)      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.2/amd64/linux-headers-5.10.2-051002-generic_5.10.2-051002.202012210832_amd64.deb > /dev/null
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.2/amd64/linux-headers-5.10.2-051002_5.10.2-051002.202012210832_all.deb > /dev/null
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.2/amd64/linux-image-unsigned-5.10.2-051002-generic_5.10.2-051002.202012210832_amd64.deb > /dev/null
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.2/amd64/linux-modules-5.10.2-051002-generic_5.10.2-051002.202012210832_amd64.deb > /dev/null
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/amd64/linux-headers-5.10.3-051003-generic_5.10.3-051003.202012261630_amd64.deb > /dev/null
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/amd64/linux-headers-5.10.3-051003_5.10.3-051003.202012261630_all.deb > /dev/null
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/amd64/linux-image-unsigned-5.10.3-051003-generic_5.10.3-051003.202012261630_amd64.deb > /dev/null
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/amd64/linux-image-unsigned-5.10.3-051003-generic_5.10.3-051003.202012261630_amd64.deb > /dev/null
 echo "* Step 2 : Updating Linux kernel...(this will take a couple of minutes)    *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
