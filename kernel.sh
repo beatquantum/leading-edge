045cb716f6b0e221431e73185706b7e92a61dec9
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-01-16 20:56:51 +0000

    Update Linux Kernel to the latest 5.11-rc-3

--- kernel.sh
+++ kernel.sh
@@ -9,17 +9,17 @@ echo "*******************************************************************"
 echo "* Your current kernel is below                                    *"
 uname -a
 echo "*******************************************************************"
-echo "* Updating to Linux kernel version 5.10.6                          *"
+echo "* Updating to Linux kernel version 5.11 RC-3                          *"
 echo "* Enter 1 to continue, or Ctrl-C to abort                         *"
 read n
 if [ $n -eq 1 ];
 then
 cd /tmp > /dev/null
 echo "* Step 1 : Downloading Linux kernel files...(this will take a minute)      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.6/amd64/linux-headers-5.10.6-051006-generic_5.10.6-051006.202101091334_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.6/amd64/linux-headers-5.10.6-051006_5.10.6-051006.202101091334_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.6/amd64/linux-image-unsigned-5.10.6-051006-generic_5.10.6-051006.202101091334_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.6/amd64/linux-modules-5.10.6-051006-generic_5.10.6-051006.202101091334_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc3/amd64/linux-headers-5.11.0-051100rc3-generic_5.11.0-051100rc3.202101102331_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc3/amd64/linux-headers-5.11.0-051100rc3_5.11.0-051100rc3.202101102331_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc3/amd64/linux-image-unsigned-5.11.0-051100rc3-generic_5.11.0-051100rc3.202101102331_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc3/amd64/linux-modules-5.11.0-051100rc3-generic_5.11.0-051100rc3.202101102331_amd64.deb
 echo "* Step 2 : Updating Linux kernel...(this will take a couple of minutes)    *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
