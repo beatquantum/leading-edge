b93e50b73be1effbd31e55a0f5d74dce28db3952
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-01-31 19:23:55 +0000

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -9,17 +9,17 @@ echo "*******************************************************************"
 echo "* Your current kernel is below                                    *"
 uname -a
 echo "*******************************************************************"
-echo "* Updating to Linux kernel version (1) 5.11 RC-5 OR (2) 5.10.10   *"
+echo "* Updating to Linux kernel version (1) 5.11 RC-5 OR (2) 5.10.12   *"
 echo "* Enter 1 or 2 to continue, or Ctrl-C to abort                    *"
 read n
 if [ $n -eq 1 ];
 then
 cd /tmp > /dev/null
 echo "* Step 1 : Downloading Linux kernel files...(this will take a minute)      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc5/amd64/linux-headers-5.11.0-051100rc5-generic_5.11.0-051100rc5.202101242134_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc5/amd64/linux-headers-5.11.0-051100rc5_5.11.0-051100rc5.202101242134_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc5/amd64/linux-image-unsigned-5.11.0-051100rc5-generic_5.11.0-051100rc5.202101242134_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc5/amd64/linux-modules-5.11.0-051100rc5-generic_5.11.0-051100rc5.202101242134_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.12/amd64/linux-headers-5.10.12-051012-generic_5.10.12-051012.202101301330_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.12/amd64/linux-headers-5.10.12-051012_5.10.12-051012.202101301330_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.12/amd64/linux-image-unsigned-5.10.12-051012-generic_5.10.12-051012.202101301330_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.12/amd64/linux-modules-5.10.12-051012-generic_5.10.12-051012.202101301330_amd64.deb
 echo "* Step 2 : Updating Linux kernel 5.11-rc5 (patience!)             *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
