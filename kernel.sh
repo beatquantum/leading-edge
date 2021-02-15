daa33bef44688e73254fe4fc6a2f752be590ce6b
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-02-15 07:55:18 +0000

    Update kernel to 5.11 or 5.10.16
    Important to test in development environment; before deployment to production environment. At the time of this update, 5.12 RC1 was not released.

--- kernel.sh
+++ kernel.sh
@@ -9,18 +9,18 @@ echo "*******************************************************************"
 echo "* Your current kernel is below                                    *"
 uname -a
 echo "*******************************************************************"
-echo "* Updating to Linux kernel version (1) 5.11 RC-7 OR (2) 5.10.14   *"
+echo "* Updating to Linux kernel version (1) 5.11 OR (2) 5.10.16   *"
 echo "* Enter 1 or 2 to continue, or Ctrl-C to abort                    *"
 read n
 if [ $n -eq 1 ];
 then
 cd /tmp > /dev/null
-echo "* Step 1 : Downloading Linux kernel files...(this will take a minute)      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc7/amd64/linux-headers-5.11.0-051100rc7-generic_5.11.0-051100rc7.202102072330_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc7/amd64/linux-headers-5.11.0-051100rc7_5.11.0-051100rc7.202102072330_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc7/amd64/linux-image-unsigned-5.11.0-051100rc7-generic_5.11.0-051100rc7.202102072330_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11-rc7/amd64/linux-modules-5.11.0-051100rc7-generic_5.11.0-051100rc7.202102072330_amd64.deb
-echo "* Step 2 : Updating Linux kernel 5.11-rc7 (patience!)             *"
+echo "* Step 1 : Downloading Linux kernel files...                      *"
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100_5.11.0-051100.202102142330_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-image-unsigned-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
+echo "* Step 2 : Updating Linux kernel 5.11     (patience!)             *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
 echo "* Reboot at your convenience and check the kernel using uname -a  *"
@@ -29,15 +29,14 @@ fi
 if [ $n -eq 2 ];
 then
 cd /tmp > /dev/null
-echo "* Step 1 : Downloading Linux kernel files...(this will take a minute)      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.14/amd64/linux-headers-5.10.14-051014-generic_5.10.14-051014.202102071532_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.14/amd64/linux-headers-5.10.14-051014_5.10.14-051014.202102071532_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.14/amd64/linux-image-unsigned-5.10.14-051014-generic_5.10.14-051014.202102071532_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.14/amd64/linux-modules-5.10.14-051014-generic_5.10.14-051014.202102071532_amd64.deb
-echo "* Step 2 : Updating Linux kernel 5.10.14...(patience!)    *"
+echo "* Step 1 : Downloading Linux kernel files...                      *"
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.16/amd64/linux-headers-5.10.16-051016-generic_5.10.16-051016.202102131334_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.16/amd64/linux-headers-5.10.16-051016_5.10.16-051016.202102131334_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.16/amd64/linux-image-unsigned-5.10.16-051016-generic_5.10.16-051016.202102131334_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.16/amd64/linux-modules-5.10.16-051016-generic_5.10.16-051016.202102131334_amd64.deb
+echo "* Step 2 : Updating Linux kernel 5.10.16...(patience!)    *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
 echo "* Reboot at your convenience and check the kernel using uname -a  *"
 echo "*******************************************************************"
 fi
-
