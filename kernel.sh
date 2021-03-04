4159544272187cded679193062024d3af94b7676
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-03-04 08:13:43 +0000

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -9,18 +9,18 @@ echo "*******************************************************************"
 echo "* Your current kernel is below                                    *"
 uname -a
 echo "*******************************************************************"
-echo "* Updating to Linux kernel version (1) 5.11 OR (2) 5.10.16   *"
+echo "* Updating to Linux kernel version (1) 5.11.2 OR (2) 5.10.16   *"
 echo "* Enter 1 or 2 to continue, or Ctrl-C to abort                    *"
 read n
 if [ $n -eq 1 ];
 then
 cd /tmp > /dev/null
 echo "* Step 1 : Downloading Linux kernel files...                      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100_5.11.0-051100.202102142330_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-image-unsigned-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
-echo "* Step 2 : Updating Linux kernel 5.11     (patience!)             *"
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.2/amd64/linux-headers-5.11.2-051102-generic_5.11.2-051102.202103030902_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.2/amd64/linux-headers-5.11.2-051102_5.11.2-051102.202103030902_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.2/amd64/linux-image-unsigned-5.11.2-051102-generic_5.11.2-051102.202103030902_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.2/amd64/linux-modules-5.11.2-051102-generic_5.11.2-051102.202103030902_amd64.deb
+echo "* Step 2 : Updating Linux kernel 5.11.2   (patience!)             *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
 echo "* Reboot at your convenience and check the kernel using uname -a  *"
