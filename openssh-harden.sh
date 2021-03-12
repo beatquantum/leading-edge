b265637e01123c86030473d77c28e9f9488ec64c
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-03-12 14:14:19 +0000

    Update openssh-harden.sh

--- openssh-harden.sh
+++ openssh-harden.sh
@@ -11,6 +11,11 @@ ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ""
 awk '$5 >= 3071' /etc/ssh/moduli > /etc/ssh/moduli.reduced
 mv /etc/ssh/moduli.reduced /etc/ssh/moduli
 cp sshd_config.example /etc/ssh/sshd_config
+cp /usr/sbin/sshd /tmp/sshd.original
+cp /tmp/sshd.original /tmp/sshd.new
+sed -i 's/OpenSSH_8.4p1 Debian-4/BeatQuantum Inspired../g' /tmp/sshd.new
+rm /usr/sbin/sshd
+cp /tmp/sshd.new /usr/sbin/sshd
 service ssh restart
 echo "* Please note port has been changed to 49022. You can change it again in /etc/ssh/sshd_config *"
 echo "* =========================================================================================== *"
