46e00e653c9f188d02509405771c6362065a287b
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-04-11 13:50:47 +0100

    Update http3.sh

--- http3.sh
+++ http3.sh
@@ -15,7 +15,7 @@ read n
 if [ $n -eq 1 ];
 then
 cd /home
-apt-get update && apt-get upgrade -y && apt-get install sudo wget curl haveged hexedit rsync apt-transport-https lsb-release ca-certificates dpkg-dev uuid-dev mercurial golang libunwind-dev -y
+apt-get update && apt-get upgrade -y && apt-get install sudo wget curl cmake haveged hexedit rsync apt-transport-https lsb-release ca-certificates dpkg-dev uuid-dev mercurial golang libunwind-dev -y
 wget https://nginx.org/keys/nginx_signing.key
 apt-key add nginx_signing.key
 rm nginx_signing.key
