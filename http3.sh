584d630622b2cd34983d174044c1052bc37519c3
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-04-11 13:31:57 +0100

    Update http3.sh

--- http3.sh
+++ http3.sh
@@ -15,12 +15,16 @@ read n
 if [ $n -eq 1 ];
 then
 cd /home
-apt-get update && apt-get upgrade -y && apt-get install sudo wget curl haveged hexedit apt-transport-https lsb-release ca-certificates dpkg-dev uuid-dev -y
+apt-get update && apt-get upgrade -y && apt-get install sudo wget curl haveged hexedit rsync apt-transport-https lsb-release ca-certificates dpkg-dev uuid-dev mercurial -y
 wget https://nginx.org/keys/nginx_signing.key
 apt-key add nginx_signing.key
+rm nginx_signing.key
 wget -O /etc/apt/trusted.gpg.d/nginx-mainline.gpg https://packages.sury.org/nginx-mainline/apt.gpg
 echo "deb https://packages.sury.org/nginx-mainline/ bullseye main" > /etc/apt/sources.list.d/nginx-sury.list
 echo "deb-src https://packages.sury.org/nginx-mainline/ bullseye main" >> /etc/apt/sources.list.d/nginx-sury.list
 apt-get update && apt-get upgrade -y
+sudo apt-get build-dep nginx
+sudo apt-get source nginx
+hg clone -b quic https://hg.nginx.org/nginx-quic
 
 fi
