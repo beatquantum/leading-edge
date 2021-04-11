afba87207319ad7ff3decf190e5c0b3192badbee
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-04-11 13:21:02 +0100

    Update http3.sh

--- http3.sh
+++ http3.sh
@@ -15,11 +15,14 @@ read n
 if [ $n -eq 1 ];
 then
 cd 
-echo "deb https://nginx.org/packages/mainline/debian testing nginx" >> /etc/apt/sources.list.d/nginx-testing.list
 ~/
 apt-get update && apt-get upgrade -y && apt-get install dpkg-dev uuid-dev -y
 wget https://nginx.org/keys/nginx_signing.key
 apt-key add nginx_signing.key
-
+apt-get -y install apt-transport-https lsb-release ca-certificates curl
+wget -O /etc/apt/trusted.gpg.d/nginx-mainline.gpg https://packages.sury.org/nginx-mainline/apt.gpg
+echo "deb https://packages.sury.org/nginx-mainline/ bullseye main" > /etc/apt/sources.list.d/nginx-sury.list
+echo "deb-src https://packages.sury.org/nginx-mainline/ bullseye main" >> /etc/apt/sources.list.d/nginx-sury.list
+apt-get update
 
 fi
