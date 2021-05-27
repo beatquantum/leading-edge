2450e714ba881f58330e03fdc06e40da8b2ec927
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-05-27 07:42:34 +0100

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -9,18 +9,18 @@ echo "*******************************************************************"
 echo "* Your current kernel is below                                    *"
 uname -a
 echo "*******************************************************************"
-echo "* Updating to Linux kernel version (1) 5.11.17 OR (2) 5.12.4        *"
+echo "* Updating to Linux kernel version (1) 5.13.RC2 OR (2) 5.12.4        *"
 echo "* Enter 1 or 2 to continue, or Ctrl-C to abort                    *"
 read n
 if [ $n -eq 1 ];
 then
 cd /tmp > /dev/null
 echo "* Step 1 : Downloading Linux kernel files...                      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.17/amd64/linux-headers-5.11.17-051117-generic_5.11.17-051117.202104281235_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.17/amd64/linux-headers-5.11.17-051117_5.11.17-051117.202104281235_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.17/amd64/linux-image-unsigned-5.11.17-051117-generic_5.11.17-051117.202104281235_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.17/amd64/linux-modules-5.11.17-051117-generic_5.11.17-051117.202104281235_amd64.deb
-echo "* Step 2 : Updating Linux kernel 5.11.17   (patience!)             *"
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13-rc2/amd64/linux-headers-5.13.0-051300rc2-generic_5.13.0-051300rc2.202105162330_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13-rc2/amd64/linux-headers-5.13.0-051300rc2_5.13.0-051300rc2.202105162330_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13-rc2/amd64/linux-image-unsigned-5.13.0-051300rc2-generic_5.13.0-051300rc2.202105162330_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13-rc2/amd64/linux-modules-5.13.0-051300rc2-generic_5.13.0-051300rc2.202105162330_amd64.deb
+echo "* Step 2 : Updating Linux kernel 5.13.rc2   (patience!)             *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
 echo "* Reboot at your convenience and check the kernel using uname -a  *"
