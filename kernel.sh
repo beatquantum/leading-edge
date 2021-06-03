e33d19ae885c5efc7d2898091572faa81185a24f
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-06-03 17:49:24 +0100

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -9,7 +9,7 @@ echo "*******************************************************************"
 echo "* Your current kernel is below                                    *"
 uname -a
 echo "*******************************************************************"
-echo "* Updating to Linux kernel version (1) 5.13.RC2 OR (2) 5.12.4        *"
+echo "* Updating to Linux kernel version (1) 5.13.RC2 OR (2) 5.12.9        *"
 echo "* Enter 1 or 2 to continue, or Ctrl-C to abort                    *"
 read n
 if [ $n -eq 1 ];
@@ -30,11 +30,11 @@ if [ $n -eq 2 ];
 then
 cd /tmp > /dev/null
 echo "* Step 1 : Downloading Linux kernel files...                      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.4/amd64/linux-headers-5.12.4-051204-generic_5.12.4-051204.202105140931_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.4/amd64/linux-headers-5.12.4-051204_5.12.4-051204.202105140931_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.4/amd64/linux-image-unsigned-5.12.4-051204-generic_5.12.4-051204.202105140931_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.4/amd64/linux-modules-5.12.4-051204-generic_5.12.4-051204.202105140931_amd64.deb
-echo "* Step 2 : Updating Linux kernel 5.12.4           ...(patience!)    *"
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.9/amd64/linux-headers-5.12.9-051209-generic_5.12.9-051209.202106030850_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.9/amd64/linux-headers-5.12.9-051209_5.12.9-051209.202106030850_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.9/amd64/linux-image-unsigned-5.12.9-051209-generic_5.12.9-051209.202106030850_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.9/amd64/linux-modules-5.12.9-051209-generic_5.12.9-051209.202106030850_amd64.deb
+echo "* Step 2 : Updating Linux kernel 5.12.9           ...(patience!)    *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
 echo "* Reboot at your convenience and check the kernel using uname -a  *"
