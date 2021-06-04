c2f4a701faf6d078883aa2394d2d27ff89437ed9
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-06-04 14:55:08 +0100

    Create install-basic-programs.sh

---
+++ install-basic-programs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+echo "*******************************************************************"
+echo "* Always use every script thoroughly in a test environment first. *"
+echo "*******************************************************************"
+echo "* Source: https://github.com/beatquantum/leading-edge.git         *"
+echo "* Author: Santosh Pandit                                          *"
+echo "*******************************************************************"
+apt-get update
+apt-get install sudo wget git haveged hexedit curl software-properties-common ufw build-essential tree nmap net-tools -y
+echo "* All done                                                        *"
