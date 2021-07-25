3cbc46cad453f06db1b90de2850667ee786baee7
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2021-07-25 07:24:13 +0100

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -9,18 +9,18 @@ echo "*******************************************************************"
 echo "* Your current kernel is below                                    *"
 uname -a
 echo "*******************************************************************"
-echo "* Updating to Linux kernel version (1) 5.13.RC2 OR (2) 5.12.12    *"
+echo "* Updating to Linux kernel version (1) 5.14-rc2 OR (2) 5.13.4    *"
 echo "* Enter 1 or 2 to continue, or Ctrl-C to abort                    *"
 read n
 if [ $n -eq 1 ];
 then
 cd /tmp > /dev/null
 echo "* Step 1 : Downloading Linux kernel files...                      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13-rc2/amd64/linux-headers-5.13.0-051300rc2-generic_5.13.0-051300rc2.202105162330_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13-rc2/amd64/linux-headers-5.13.0-051300rc2_5.13.0-051300rc2.202105162330_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13-rc2/amd64/linux-image-unsigned-5.13.0-051300rc2-generic_5.13.0-051300rc2.202105162330_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13-rc2/amd64/linux-modules-5.13.0-051300rc2-generic_5.13.0-051300rc2.202105162330_amd64.deb
-echo "* Step 2 : Updating Linux kernel 5.13.rc2   (patience!)             *"
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14-rc2/amd64/linux-headers-5.14.0-051400rc2-generic_5.14.0-051400rc2.202107182230_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14-rc2/amd64/linux-headers-5.14.0-051400rc2_5.14.0-051400rc2.202107182230_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14-rc2/amd64/linux-image-unsigned-5.14.0-051400rc2-generic_5.14.0-051400rc2.202107182230_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.14-rc2/amd64/linux-modules-5.14.0-051400rc2-generic_5.14.0-051400rc2.202107182230_amd64.deb
+echo "* Step 2 : Updating Linux kernel 5.14-rc2 (patience!)             *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
 echo "* Reboot at your convenience and check the kernel using uname -a  *"
@@ -30,11 +30,11 @@ if [ $n -eq 2 ];
 then
 cd /tmp > /dev/null
 echo "* Step 1 : Downloading Linux kernel files...                      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.12/amd64/linux-headers-5.12.12-051212-generic_5.12.12-051212.202106180931_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.12/amd64/linux-headers-5.12.12-051212_5.12.12-051212.202106180931_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.12/amd64/linux-image-unsigned-5.12.12-051212-generic_5.12.12-051212.202106180931_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.12/amd64/linux-modules-5.12.12-051212-generic_5.12.12-051212.202106180931_amd64.deb
-echo "* Step 2 : Updating Linux kernel 5.12.12        ...(patience!)    *"
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.4/amd64/linux-headers-5.13.4-051304-generic_5.13.4-051304.202107201535_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.4/amd64/linux-headers-5.13.4-051304_5.13.4-051304.202107201535_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.4/amd64/linux-image-unsigned-5.13.4-051304-generic_5.13.4-051304.202107201535_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.13.4/amd64/linux-modules-5.13.4-051304-generic_5.13.4-051304.202107201535_amd64.deb
+echo "* Step 2 : Updating Linux kernel 5.13.4        ...(patience!)    *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
 echo "* Reboot at your convenience and check the kernel using uname -a  *"
