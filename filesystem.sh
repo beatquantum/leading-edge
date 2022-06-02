642a9075ec56cfa551ac1c0778210331be1b33e2
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2022-06-02 03:48:02 +0000

    Update filesystem.sh

--- filesystem.sh
+++ filesystem.sh
@@ -20,6 +20,7 @@ echo "install hfsplus /bin/true" >> /etc/modprobe.d/blacklist.conf
 echo "install jffs2 /bin/true" >> /etc/modprobe.d/blacklist.conf
 echo "install udf /bin/true" >> /etc/modprobe.d/blacklist.conf
 echo "blacklist usb-storage" >> /etc/modprobe.d/blacklist.conf
+echo "blacklist firewire-ohci" >> /etc/modprobe.d/blacklist.conf
 echo "install dccp /bin/true" >> /etc/modprobe.d/blacklist.conf
 echo "install sctp /bin/true" >> /etc/modprobe.d/blacklist.conf
 echo "install rds /bin/true" >> /etc/modprobe.d/blacklist.conf
