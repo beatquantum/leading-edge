e5a1a866cd28205ff76e46bfa8595f3b772b7104
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2022-06-02 03:40:18 +0000

    Update all.sh

--- all.sh
+++ all.sh
@@ -5,5 +5,24 @@
 # "* Source: https://github.com/beatquantum/leading-edge.git         *"
 # "* Author: Santosh Pandit                                          *"
 # "*******************************************************************"
-# "* This script clears Linux logs and journal                       *"
-
+echo "*******************************************************************"
+echo "* Basic Stuff                                                     *"
+apt-get update
+apt-get upgrade -y
+echo "*******************************************************************"
+echo "* Do you wish to use Debian Unstable?                             *"
+echo "* Enter 1 or 2 to skip, or Ctrl-C to abort                        *"
+read n
+if [ $n -eq 1 ];
+then
+sh unstable.sh
+fi
+echo "*******************************************************************"
+echo "* Do you wish to use sshd_config prototoype                       *"
+echo "* Enter 1 or 2 to skip, or Ctrl-C to abort                        *"
+read n
+if [ $n -eq 1 ];
+then
+cp sshd_config_example /etc/ssh/sshd_config
+fi
+echo "*******************************************************************"
