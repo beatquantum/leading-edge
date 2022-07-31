b5366b3a0c4b18ec6ec7bce3603c8fbefa64b726
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2022-07-31 05:10:05 +0100

    Delete nginx.sh

--- nginx.sh
+++
@@ -1,6 +0,0 @@
-!/bin/bash
-apt install haveged hexedit curl sudo apt-transport-https lsb-release ca-certificates curl gnupg -y
-curl https://mirror-cdn.xtom.com/sb/nginx/public.key | apt-key add -
-echo "deb https://mirror-cdn.xtom.com/sb/nginx/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/sb-nginx.list
-apt update
-apt install nginx-extras -y
