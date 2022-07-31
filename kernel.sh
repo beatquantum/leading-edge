26aa82e8c626ccfe828edbe4a6642fec9558eae6
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2022-07-31 05:48:15 +0100

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -9,33 +9,19 @@ echo "*******************************************************************"
 echo "* Your current kernel is below                                    *"
 uname -a
 echo "*******************************************************************"
-echo "* Updating to Linux kernel version (1) 5.16.3 OR (2) none    *"
-echo "* Enter 1 or 2 to continue, or Ctrl-C to abort                    *"
+echo "* Updating to Linux kernel version 5.18.15                        *"
+echo "* Enter 1 to continue, or Ctrl-C to abort                         *"
 read n
 if [ $n -eq 1 ];
 then
 cd /tmp > /dev/null
 echo "* Step 1 : Downloading Linux kernel files...                      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.3/amd64/linux-headers-5.16.3-051603-generic_5.16.3-051603.202201270732_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.3/amd64/linux-headers-5.16.3-051603_5.16.3-051603.202201270732_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.3/amd64/linux-image-unsigned-5.16.3-051603-generic_5.16.3-051603.202201270732_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.3/amd64/linux-modules-5.16.3-051603-generic_5.16.3-051603.202201270732_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-headers-5.18.15-051815-generic_5.18.15-051815.202207291649_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-headers-5.18.15-051815_5.18.15-051815.202207291649_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-image-unsigned-5.18.15-051815-generic_5.18.15-051815.202207291649_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-modules-5.18.15-051815-generic_5.18.15-051815.202207291649_amd64.deb
 
-echo "* Step 2 : Updating Linux kernel 5.16.3 (patience!)             *"
-dpkg -i *.deb
-echo "* Linux kernel has been updated.                                  *"
-echo "* Reboot at your convenience and check the kernel using uname -a  *"
-echo "*******************************************************************"
-fi
-if [ $n -eq 2 ];
-then
-cd /tmp > /dev/null
-echo "* Step 1 : Downloading Linux kernel files...                      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.3/amd64/linux-headers-5.16.3-051603-generic_5.16.3-051603.202201270732_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.3/amd64/linux-headers-5.16.3-051603_5.16.3-051603.202201270732_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.3/amd64/linux-image-unsigned-5.16.3-051603-generic_5.16.3-051603.202201270732_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.3/amd64/linux-modules-5.16.3-051603-generic_5.16.3-051603.202201270732_amd64.deb
-echo "* Step 2 : Updating Linux kernel 5.16.3        ...(patience!)    *"
+echo "* Step 2 : Updating Linux kernel (patience!)                      *"
 dpkg -i *.deb
 echo "* Linux kernel has been updated.                                  *"
 echo "* Reboot at your convenience and check the kernel using uname -a  *"
