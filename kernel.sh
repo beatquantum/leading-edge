c529a1417b148182cc67d1c1a1a650710d81a0cf
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2022-12-26 10:50:23 +0000

    Kernel 6.1 RC5

--- kernel.sh
+++ kernel.sh
@@ -9,17 +9,17 @@ echo "*******************************************************************"
 echo "* Your current kernel is below                                    *"
 uname -a
 echo "*******************************************************************"
-echo "* Updating to Linux kernel version 5.18.15                        *"
+echo "* Updating to Linux kernel version 6.1 RC5                        *"
 echo "* Enter 1 to continue, or Ctrl-C to abort                         *"
 read n
 if [ $n -eq 1 ];
 then
 cd /tmp > /dev/null
 echo "* Step 1 : Downloading Linux kernel files...                      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-headers-5.18.15-051815-generic_5.18.15-051815.202207291649_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-headers-5.18.15-051815_5.18.15-051815.202207291649_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-image-unsigned-5.18.15-051815-generic_5.18.15-051815.202207291649_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-modules-5.18.15-051815-generic_5.18.15-051815.202207291649_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.1-rc5/amd64/linux-headers-6.1.0-060100rc5-generic_6.1.0-060100rc5.202211132230_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.1-rc5/amd64/linux-headers-6.1.0-060100rc5_6.1.0-060100rc5.202211132230_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.1-rc5/amd64/linux-image-unsigned-6.1.0-060100rc5-generic_6.1.0-060100rc5.202211132230_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.1-rc5/amd64/linux-modules-6.1.0-060100rc5-generic_6.1.0-060100rc5.202211132230_amd64.deb
 
 echo "* Step 2 : Updating Linux kernel (patience!)                      *"
 dpkg -i *.deb
