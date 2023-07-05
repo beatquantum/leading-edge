ca4d1f912aa0485e372dfe546c012ebc854407b5
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2023-07-05 19:12:03 +0100

    Update haveged.sh

--- haveged.sh
+++ haveged.sh
@@ -7,14 +7,23 @@
 # "*******************************************************************"
 # "* Install haveged to boost entropy for random number generation   *"
 # "*******************************************************************"
+
 echo "Running apt-get update"
 apt-get update > /dev/null
+
 echo "Installing haveged and rng-tools"
 apt-get install haveged rng-tools -y > /dev/null
-echo "Changing default DAEMON_ARGS to -w 4096"
-sed -i 's/\#DAEMON_ARGS=\"\"/DAEMON_ARGS=\"-w 4096\"/g' /etc/default/haveged
-echo "Setting haveged to run automatically"
-update-rc.d haveged defaults
-echo "Running FIPS rng test 10,000 times"
-cat /dev/random | rngtest -c 10000
-echo "I hope you did not get more than 10 errors above"
+
+echo "Configuring haveged"
+sed -i 's/^# DAEMON_ARGS=.*/DAEMON_ARGS="-w 4096"/' /etc/default/haveged
+
+echo "Enabling haveged to run at boot"
+systemctl enable haveged
+
+echo "Testing the entropy pool using rngtest"
+rngtest -c 10000 > /dev/null 2>&1
+if [ "$?" -gt 0 ]; then
+    echo "Warning: rngtest detected insufficient entropy."
+else
+    echo "Entropy test passed. Random number generation improved."
+fi
