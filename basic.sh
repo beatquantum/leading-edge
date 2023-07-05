01e1b48e22980f8ae85068a9620d63e84db881ef
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2023-07-05 19:05:21 +0100

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+
 clear
+
 echo "*******************************************************************"
 echo "* Always use every script thoroughly in a test environment first. *"
 echo "*******************************************************************"
 echo "* Source: https://github.com/beatquantum/leading-edge.git         *"
 echo "* Author: Santosh Pandit                                          *"
 echo "*******************************************************************"
+
+# Update package lists
 apt-get update
-apt-get install sudo wget git haveged hexedit curl software-properties-common make ufw build-essential tree nmap net-tools qrencode usbguard -y
+
+# Install essential packages
+apt-get install -y sudo wget git haveged hexedit curl software-properties-common make ufw build-essential tree nmap net-tools qrencode usbguard
+
 echo "* All done                                                        *"
