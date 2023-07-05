0af61c153f495e44ddba74858387f15b97b417c2
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2023-07-05 19:08:19 +0100

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
+
 clear
+
 echo "*******************************************************************"
 echo "* Always use every script thoroughly in a test environment first. *"
 echo "*******************************************************************"
 echo "* Source: https://github.com/beatquantum/leading-edge.git         *"
 echo "* Author: Santosh Pandit                                          *"
 echo "*******************************************************************"
-echo "* The script will disable kernel dumps and make misc improvements *"
-echo "* Enter 1 to continue, or Ctrl-C to abort                         *"
-read n
-if [ $n -eq 1 ];
-then
-echo "Disabling kernel dumps and setting timeout to 1 hour"
-echo "ulimit -c 0 > /dev/null 2>&1" > /etc/profile.d/disable-coredumps.sh
-echo "TMOUT=3600" >> /etc/profile
-echo "readonly TMOUT" >> /etc/profile
-echo "export TMOUT" >> /etc/profile
-echo "* hard core 0" >> /etc/security/limits.conf
-echo "* soft core 0" >> /etc/security/limits.conf
-echo "fs.suid_dumpable=0" >> /etc/sysctl.conf
-echo "kernel.kptr_restrict=2" >> /etc/sysctl.conf
-sysctl -p > /dev/null
-echo "* All done. Reboot at your convenience for full effect.           *"
+
+echo "* The script will disable kernel dumps and make miscellaneous improvements *"
+echo "* Enter 1 to continue, or Ctrl-C to abort                                 *"
+read -r n
+
+if [ "$n" -eq 1 ]; then
+  echo "Disabling kernel dumps and setting timeout to 1 hour"
+
+  echo "ulimit -c 0 > /dev/null 2>&1" > /etc/profile.d/disable-coredumps.sh
+
+  echo "TMOUT=3600" >> /etc/profile
+  echo "readonly TMOUT" >> /etc/profile
+  echo "export TMOUT" >> /etc/profile
+
+  echo "* hard core 0" >> /etc/security/limits.conf
+  echo "* soft core 0" >> /etc/security/limits.conf
+
+  echo "fs.suid_dumpable=0" >> /etc/sysctl.conf
+  echo "kernel.kptr_restrict=2" >> /etc/sysctl.conf
+
+  sysctl -p > /dev/null
+
+  echo "* All done. Reboot at your convenience for full effect. *"
 fi
