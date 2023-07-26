4a742ddcdb163b978fb1102cb880fcfd8a0fd893
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2023-07-26 06:38:34 +0100

    Update check-debian.sh

--- check-debian.sh
+++ check-debian.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Author: Santosh Pandit
-Purpose: To check if Debian is installed
-Last update: 26 July 2023
+# Author: Santosh Pandit
+# Purpose: To check if Debian is installed
+# Last update: 26 July 2023
 
 # Get the OS name from /etc/os-release
 os_name=$(grep -oP '(?<=^NAME=).+' /etc/os-release | tr -d '"')
