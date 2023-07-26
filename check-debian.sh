dcee28127c3202c9590d52caf8b39a8df4835cbe
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2023-07-26 06:34:37 +0100

    Create check-debian.sh

---
+++ check-debian.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+Author: Santosh Pandit
+Purpose: To check if Debian is installed
+Last update: 26 July 2023
+
+# Get the OS name from /etc/os-release
+os_name=$(grep -oP '(?<=^NAME=).+' /etc/os-release | tr -d '"')
+
+# Check if the OS is "Debian"
+if [ "$os_name" = "Debian" ]; then
+    echo "Debian is found."
+else
+    echo "Please use Debian and continue."
+fi
