98012d27e2664bf6a48b94f9f639f79f5869534e
Santosh Pandit <67234356+beatquantum@users.noreply.github.com>
2023-07-29 18:43:37 +0100

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -9,17 +9,17 @@ echo "*******************************************************************"
 echo "* Your current kernel is below                                    *"
 uname -a
 echo "*******************************************************************"
-echo "* Updating to Linux kernel version 6.4.3                          *"
+echo "* Updating to Linux kernel version 6.5-RC3                        *"
 echo "* Enter 1 to continue, or Ctrl-C to abort                         *"
 read n
 if [ $n -eq 1 ];
 then
 cd /tmp > /dev/null
 echo "* Step 1 : Downloading Linux kernel files...                      *"
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.4.3/amd64/linux-headers-6.4.3-060403-generic_6.4.3-060403.202307110536_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.4.3/amd64/linux-headers-6.4.3-060403_6.4.3-060403.202307110536_all.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.4.3/amd64/linux-image-unsigned-6.4.3-060403-generic_6.4.3-060403.202307110536_amd64.deb
-wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.4.3/amd64/linux-modules-6.4.3-060403-generic_6.4.3-060403.202307110536_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.5-rc3/amd64/linux-headers-6.5.0-060500rc3-generic_6.5.0-060500rc3.202307232333_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.5-rc3/amd64/linux-headers-6.5.0-060500rc3_6.5.0-060500rc3.202307232333_all.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.5-rc3/amd64/linux-image-unsigned-6.5.0-060500rc3-generic_6.5.0-060500rc3.202307232333_amd64.deb
+wget -c --no-check-certificate https://kernel.ubuntu.com/~kernel-ppa/mainline/v6.5-rc3/amd64/linux-modules-6.5.0-060500rc3-generic_6.5.0-060500rc3.202307232333_amd64.deb
 
 echo "* Step 2 : Updating Linux kernel (patience!)                      *"
 dpkg -i *.deb
